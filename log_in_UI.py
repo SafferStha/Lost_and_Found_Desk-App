1163b7dca9ec86e33add957ea5265195d9f03043
AayuAmor <aayuamor@gmail.com>
2025-07-17 08:57:43 +0545

    Postioning the button to the right using sticky='e' and increasing the widht of entry box

--- log_in_UI.py
+++ log_in_UI.py
@@ -11,15 +11,15 @@ login_frame.grid(row=0, column=0, padx=50, pady=170, sticky="e")
 
 # entry fields for username and password using grid within the frame
 Label(login_frame, text="Username:").grid(row=0, column=0, padx=10, pady=10, sticky="e")
-username_entry = Entry(login_frame, width=20, relief="sunken", bd=2)
-username_entry.grid(row=0, column=1, padx=10, pady=10)
+username_entry = Entry(login_frame, width=30, relief="sunken", bd=2)
+username_entry.grid(row=0, column=1, pady=10)
 
 Label(login_frame, text="Password:").grid(row=1, column=0, padx=10, pady=10, sticky="e")
-password_entry = Entry(login_frame, show="*", width=20, relief="sunken", bd=2)
-password_entry.grid(row=1, column=1, padx=10, pady=10)
+password_entry = Entry(login_frame, show="*", width=30, relief="sunken", bd=2)
+password_entry.grid(row=1, column=1, pady=10)
 
 # login button
 login_button = Button(login_frame, text="Login", width=15, relief="raised", bd=2)
-login_button.grid(row=2, column=0, columnspan=2, pady=20)
+login_button.grid(row=2, column=1, columnspan=2, pady=20, sticky="e")
 
 root.mainloop()
